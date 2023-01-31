64eae6d56eb9b45f5d52579750f9f37844c5ebf5
RobertoRodriguezNunez <roberto.s252272@cesurformacion.com>
2023-01-31 13:08:28 +0100

    Subimos la API de Swagger actualizada, junto con PUT y POST de hombres y mujeres

---
+++ src/routes/hombres.js
@@ -0,0 +1,84 @@
+var router = require('express').Router()
+const hombres = new Map();
+
+hombres.set("1", "Juan Perez");
+hombres.set("2", "Pedro García");
+hombres.set("3", "Miguel Rodriguez");
+hombres.set("4", "Carlos Martinez");
+hombres.set("5", "José Fernandez");
+hombres.set("6", "Antonio Gonzalez");
+hombres.set("7", "David López");
+hombres.set("8", "Javier Sánchez");
+hombres.set("9", "Rafael Hernández");
+hombres.set("10", "Francisco Díaz");
+hombres.set("11", "Diego Moreno");
+hombres.set("12", "Sergio Pérez");
+hombres.set("13", "Luis Castro");
+hombres.set("14", "Alberto Jiménez");
+hombres.set("15", "Mario Gómez");
+hombres.set("16", "Manuel González");
+hombres.set("17", "Andrés Fernández");
+hombres.set("18", "Jorge García");
+hombres.set("19", "Lucas Ortiz");
+hombres.set("20", "Alejandro Rodríguez");
+hombres.set("21", "José Manuel Silva");
+hombres.set("22", "Daniel Ruiz");
+hombres.set("23", "Ángel Vásquez");
+hombres.set("24", "Isaac Muñoz");
+hombres.set("25", "Enrique Romero");
+hombres.set("26", "Juan Carlos Martínez");
+hombres.set("27", "Juan José García");
+hombres.set("28", "Ricardo López");
+hombres.set("29", "Julián Fernández");
+hombres.set("30", "Pablo Hernández");
+hombres.set("31", "Mauricio Sánchez");
+hombres.set("32", "Eduardo Rodriguez");
+hombres.set("33", "Marcos González");
+hombres.set("34", "Víctor Ortiz");
+hombres.set("35", "Rubén García");
+hombres.set("36", "Gerardo López");
+hombres.set("37", "Federico Pérez");
+hombres.set("38", "Gustavo Martínez");
+hombres.set("39", "Esteban Ruiz");
+hombres.set("40", "Raúl Fernández");
+hombres.set("41", "Roberto Hernández");
+hombres.set("42", "Santiago González");
+hombres.set("43", "Adrián Rodriguez");
+hombres.set("44", "Tomás García");
+hombres.set("45", "Salvador Ortiz");
+hombres.set("46", "Joaquín Sánchez");
+hombres.set("47", "Oscar Muñoz");
+hombres.set("48", "Aitor Fernández");
+hombres.set("49", "Álvaro García");
+hombres.set("50", "Francisco Gómez");
+
+
+router.get('/:id', function(req, res) {
+var nombre = hombres.get(req.params.id);
+
+if (nombre==null)
+{ res.json({ message: "No existe ningún hombre con id " + req.params.id });
+}
+  else {res.json({ name: nombre })
+}})
+
+router.get('/', function(req, res) {
+    array = Array.from(hombres, ([id, name]) => ({ id, name }));
+    console.log(array);
+    res.json(array)
+})
+
+router.post('/', function(req, res) {
+  res.json({ message: 'Vas a añadir un hombre' })
+})
+
+router.put('/:id', function(req, res) {
+  res.json({ message: 'Vas a actualizar el hombre con id ' + req.params.id })
+})
+
+router.delete('/:id', function(req, res) {
+  res.json({ message: 'Vas a borrar el hombre con id ' + req.params.id})
+})
+
+
+module.exports = router
---
+++ src/routes/mujeres.js
@@ -0,0 +1,80 @@
+var router = require('express').Router()
+const mujeres = new Map();
+
+mujeres.set("1", "María García");
+mujeres.set("2", "Ana Rodríguez");
+mujeres.set("3", "Isabel Fernández");
+mujeres.set("4", "Lucía González");
+mujeres.set("5", "Sofía Martínez");
+mujeres.set("6", "Marta Pérez");
+mujeres.set("7", "Elena López");
+mujeres.set("8", "Rosa Sánchez");
+mujeres.set("9", "Carmen Romero");
+mujeres.set("10", "Julia Ortiz");
+mujeres.set("11", "Teresa Jiménez");
+mujeres.set("12", "Clara Alonso");
+mujeres.set("13", "Beatriz Moya");
+mujeres.set("14", "Emilia Navarro");
+mujeres.set("15", "Dolores Moreno");
+mujeres.set("16", "Esther Rodríguez");
+mujeres.set("17", "Verónica Castro");
+mujeres.set("18", "Aurora Sanz");
+mujeres.set("19", "Laura Díaz");
+mujeres.set("20", "Miriam Pérez");
+mujeres.set("21", "Silvia Gómez");
+mujeres.set("22", "Alba Moreno");
+mujeres.set("23", "Adriana García");
+mujeres.set("24", "Mercedes Sánchez");
+mujeres.set("25", "Concepción Martínez");
+mujeres.set("26", "Rita Fernández");
+mujeres.set("27", "Alicia Ortiz");
+mujeres.set("28", "Carlota González");
+mujeres.set("29", "Esperanza Romero");
+mujeres.set("30", "Elvira López");
+mujeres.set("31", "Fátima Rodríguez");
+mujeres.set("32", "Inés Jiménez");
+mujeres.set("33", "Irene Díaz");
+mujeres.set("34", "Juliana Pérez");
+mujeres.set("35", "Lorena Gómez");
+mujeres.set("36", "Marisol Moreno");
+mujeres.set("37", "Nuria García");
+mujeres.set("38", "Olga Sánchez");
+mujeres.set("39", "Patricia Martínez");
+mujeres.set("40", "Ramona Fernández");
+mujeres.set("41", "Raquel Ortiz");
+mujeres.set("42", "Rebeca González");
+mujeres.set("43", "Rocío Romero");
+mujeres.set("44", "Sara López");
+mujeres.set("45", "Sofía Rodríguez");
+mujeres.set("46", "Julia López");
+mujeres.set("47", "Susana Jiménez");
+mujeres.set("48", "Catalina Domínguez");
+mujeres.set("49", "María de la Paz Vázquez");
+mujeres.set("50", "Agueda Ruiz");
+
+
+router.get('/:id', function(req, res) {
+
+  res.json({ message: mujeres.get(req.params.id) })
+})
+
+router.get('/', function(req, res) {
+    array = Array.from(mujeres, ([name, value]) => ({ name, value }));
+    console.log(array);
+    res.json(array)
+})
+
+router.post('/', function(req, res) {
+  res.json({ message: 'Vas a añadir una mujer' })
+})
+
+router.put('/:id', function(req, res) {
+  res.json({ message: 'Vas a actualizar la mujer con id ' + req.params.id })
+})
+
+router.delete('/:id', function(req, res) {
+  res.json({ message: 'Vas a borrar la mujer con id ' + req.params.id})
+})
+
+
+module.exports = router
